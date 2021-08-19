b1e45d8a3682e54e4bf5280ca138e486c45f5391
Yatharth Nigam <yatharthnigam@gmail.com>
2021-08-19 09:52:02 +0530

    functionality added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    setItem(event.target.value);
+    console.log(item);
+  }
+
+  function handleClick() {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+
+    setItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={item} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
